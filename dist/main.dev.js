f5fbdc86db4c597a3ea8ba684b530c30ceabed94
Ocrown <tuliocrown@gmail.com>
2022-06-11 18:13:01 -0400

    eventos2.0

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -2,18 +2,19 @@
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var Hamburguesa = function Hamburguesa(nombre, descripcion, precio) {
+var Hamburguesa = function Hamburguesa(id, nombre, descripcion, precio) {
   _classCallCheck(this, Hamburguesa);
 
+  this.id = id;
   this.nombre = nombre;
   this.descripcion = descripcion;
   this.precio = precio;
 };
 
-var hamburguesa1 = new Hamburguesa("hamburguesa de carne", "pan, carne, tomate, lechuga, salsas", 7990);
-var hamburguesa2 = new Hamburguesa("hamburguesa de pollo", "pan, pollo, tomate, lechuga, salsas", 6990);
-var hamburguesa3 = new Hamburguesa("hamburguesa de chuleta", "pan, chuleta, tomate, lechuga, salsas", 8990);
-var hamburguesa4 = new Hamburguesa("hamburguesa mixta", "pan, carne, pollo y/o chuleta, tomate, lechuga, salsas", 10990);
+var hamburguesa1 = new Hamburguesa(101, "hamburguesa de carne", "pan, carne, tomate, lechuga, salsas", 7990);
+var hamburguesa2 = new Hamburguesa(102, "hamburguesa de pollo", "pan, pollo, tomate, lechuga, salsas", 6990);
+var hamburguesa3 = new Hamburguesa(103, "hamburguesa de chuleta", "pan, chuleta, tomate, lechuga, salsas", 8990);
+var hamburguesa4 = new Hamburguesa(104, "hamburguesa mixta", "pan, carne, pollo y/o chuleta, tomate, lechuga, salsas", 10990);
 var carrito = [];
 var productos = [hamburguesa1, hamburguesa2, hamburguesa3, hamburguesa4];
 
@@ -38,46 +39,19 @@ var cardContainer = document.querySelector("#cajonContenedor");
 productos.forEach(function (producto) {
   var cardP = document.createElement("div");
   cardP.className = "card col-xs-12 col-lg-3 col-sm-3";
-  cardP.innerHTML = "  <p class=\"card-text\">".concat(producto.nombre, "</p>\n     <img src=\"../media/burguer.jpg\" class=\"p-2 card-img-top\" alt=\"...\">\n    <div class=\"card-body\">\n      <p class=\"card-text\">Ingredientes: ").concat(producto.descripcion, "</p>\n      <p class=\"card-text\">$").concat(producto.precio, "</p>\n      <a href=\"#\" class=\"btn btn-warning\">Agregar</a>");
+  cardP.innerHTML = "  <p class=\"card-text\">".concat(producto.nombre, "</p>\n     <img src=\"../media/burguer.jpg\" class=\"p-2 card-img-top\" alt=\"...\">\n    <div class=\"card-body\">\n      <p class=\"card-text\">Ingredientes: ").concat(producto.descripcion, "</p>\n      <p class=\"card-text\">$").concat(producto.precio, "</p>\n      <button data-id=").concat(producto.id, " class=\"btn btn-warning\">Agregar</button>");
   cardContainer.append(cardP);
-});
-console.log(productos);
+}); //
 
-var agregarProducto = function agregarProducto() {
-  var productoAgregado = prompt("Elige tu hamburguesa! tenemos de carne, pollo, chuleta o mixta").toLowerCase();
-
-  switch (productoAgregado) {
-    case "carne":
-      mostrarMensaje(hamburguesa1);
-      carrito.push(hamburguesa1);
-      break;
-
-    case "pollo":
-      mostrarMensaje(hamburguesa2);
-      carrito.push(hamburguesa2);
-      break;
-
-    case "chuleta":
-      mostrarMensaje(hamburguesa3);
-      carrito.push(hamburguesa3);
-      break;
-
-    case "mixta":
-      mostrarMensaje(hamburguesa4);
-      carrito.push(hamburguesa4);
-      break;
-
-    default:
-      alert("Lo sentimos, este producto no se encuentra disponible. Por favor elige uno de nuestro stock");
-      break;
-  }
-
-  var confirmacionUsuario = confirm("desea agregar algo mas?");
-
-  if (confirmacionUsuario) {
-    agregarProducto();
-  } else {
-    alert("el total de su compra es $" + totalCarrito());
-    console.log(totalCarrito());
-  }
+var agregarProducto = function agregarProducto(e) {
+  var productoElegido = e.target.getAttribute("data-id");
+  var producto = productos.find(function (producto) {
+    return producto.nombre == productoElegido;
+  });
+  console.log(e.target);
 };
+
+var botonesDeAgregar = document.querySelectorAll(".btn");
+botonesDeAgregar.forEach(function (botonCompra) {
+  botonCompra.addEventListener("click", agregarProducto);
+});
--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 
 
 class Hamburguesa {
-    constructor(nombre, descripcion, precio) {
+    constructor(id, nombre, descripcion, precio) {
+        this.id = id
         this.nombre = nombre
         this.descripcion = descripcion
         this.precio = precio
     }
 }
 
-const hamburguesa1 = new Hamburguesa("hamburguesa de carne", "pan, carne, tomate, lechuga, salsas", 7990)
-const hamburguesa2 = new Hamburguesa("hamburguesa de pollo", "pan, pollo, tomate, lechuga, salsas", 6990)
-const hamburguesa3 = new Hamburguesa("hamburguesa de chuleta", "pan, chuleta, tomate, lechuga, salsas", 8990)
-const hamburguesa4 = new Hamburguesa("hamburguesa mixta", "pan, carne, pollo y/o chuleta, tomate, lechuga, salsas", 10990)
+const hamburguesa1 = new Hamburguesa(101, "hamburguesa de carne", "pan, carne, tomate, lechuga, salsas", 7990)
+const hamburguesa2 = new Hamburguesa(102, "hamburguesa de pollo", "pan, pollo, tomate, lechuga, salsas", 6990)
+const hamburguesa3 = new Hamburguesa(103, "hamburguesa de chuleta", "pan, chuleta, tomate, lechuga, salsas", 8990)
+const hamburguesa4 = new Hamburguesa(104, "hamburguesa mixta", "pan, carne, pollo y/o chuleta, tomate, lechuga, salsas", 10990)
 
 const carrito = []
 
@@ -21,6 +22,7 @@ const mostrarMensaje = (hamburguesa) => {
     alert(hamburguesa.nombre + " lleva " + hamburguesa.descripcion + " y tiene el valor de $" + hamburguesa.precio)
     const confirmacionUsuario2 = confirm("desea llevarla?")
     if (confirmacionUsuario2) {
+
     } else {
         agregarProducto()
     }
@@ -44,50 +46,23 @@ productos.forEach((producto) => {
     <div class="card-body">
       <p class="card-text">Ingredientes: ${producto.descripcion}</p>
       <p class="card-text">$${producto.precio}</p>
-      <a href="#" class="btn btn-warning">Agregar</a>`
+      <button data-id=${producto.id} class="btn btn-warning">Agregar</button>`
 
     cardContainer.append(cardP)
 
 })
 
+//
 
-
-console.log(productos);
-const agregarProducto = () => {
-
-    const productoAgregado = prompt("Elige tu hamburguesa! tenemos de carne, pollo, chuleta o mixta").toLowerCase()
-
-    switch (productoAgregado) {
-        case "carne":
-            mostrarMensaje(hamburguesa1)
-            carrito.push(hamburguesa1)
-            break
-        case "pollo":
-            mostrarMensaje(hamburguesa2)
-            carrito.push(hamburguesa2)
-            break
-        case "chuleta":
-            mostrarMensaje(hamburguesa3)
-            carrito.push(hamburguesa3)
-            break
-        case "mixta":
-            mostrarMensaje(hamburguesa4)
-            carrito.push(hamburguesa4)
-            break
-        default: alert("Lo sentimos, este producto no se encuentra disponible. Por favor elige uno de nuestro stock")
-            break
-
-    }
-    const confirmacionUsuario = (confirm("desea agregar algo mas?"))
-
-    if (confirmacionUsuario) {
-        agregarProducto()
-
-    } else {
-        alert("el total de su compra es $" + totalCarrito())
-        console.log(totalCarrito());
-    }
-
+const agregarProducto = (e) => {
+    const productoElegido = e.target.getAttribute("data-id")
+    const producto = productos.find((producto) => producto.nombre == productoElegido)
+    console.log(e.target);
 }
+
+const botonesDeAgregar = document.querySelectorAll(".btn")
+botonesDeAgregar.forEach((botonCompra) => {
+    botonCompra.addEventListener("click", agregarProducto)
+})
 
 
