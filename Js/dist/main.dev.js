1bc3d2a12a2c564e0dd347e84d7b68fb9920973a
Ocrown <tuliocrown@gmail.com>
2022-08-08 03:35:31 -0400

    botondeAgregar+aviso+carrito

--- Js/dist/main.dev.js
+++ Js/dist/main.dev.js
@@ -1,90 +1,102 @@
 "use strict";
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var renderizadoDeProductos = function renderizadoDeProductos(data) {
-  console.log(data);
-};
-
-fetch('../productos.json').then(function (res) {
-  return res.json();
-}).then(function (data) {
-  renderizadoDeProductos(data);
-});
-
-var Hamburguesa = function Hamburguesa(id, nombre, descripcion, precio) {
-  _classCallCheck(this, Hamburguesa);
-
-  this.id = id;
-  this.nombre = nombre;
-  this.descripcion = descripcion;
-  this.precio = precio;
-};
-
-var hamburguesa1 = new Hamburguesa(101, "hamburguesa de carne", "pan, carne, tomate, lechuga, salsas", 7990);
-var hamburguesa2 = new Hamburguesa(102, "hamburguesa de pollo", "pan, pollo, tomate, lechuga, salsas", 6990);
-var hamburguesa3 = new Hamburguesa(103, "hamburguesa de chuleta", "pan, chuleta, tomate, lechuga, salsas", 8990);
-var hamburguesa4 = new Hamburguesa(104, "hamburguesa mixta", "pan, carne, pollo y/o chuleta, tomate, lechuga, salsas", 10990);
 var carrito = [];
-var productos = [hamburguesa1, hamburguesa2, hamburguesa3, hamburguesa4];
-
-var mostrarMensaje = function mostrarMensaje() {
-  Swal.fire({
-    title: 'Desea Llevarla?',
-    showDenyButton: true,
-    showCancelButton: true,
-    confirmButtonText: 'Quiero llevarla',
-    confirmButtonColor: "#22D619",
-    denyButtonText: "Mejor no",
-    denyButtonColor: "#EA1818"
-  }).then(function (result) {
-    /* Read more about isConfirmed, isDenied below */
-    if (result.isConfirmed) {
-      Toastify({
-        text: "Producto Agregado",
-        className: "info",
-        style: {
-          background: "linear-gradient(to right, #22D619, #52D84B)"
-        }
-      }).showToast();
-    } else if (result.isDenied) {
-      Toastify({
-        text: "no se ha agregado el producto",
-        className: "info",
-        style: {
-          background: "linear-gradient(to right, #EA1818, #C83939)"
-        }
-      }).showToast();
-    }
-  });
-};
+var productos = [];
 
 var totalCarrito = function totalCarrito() {
   var sumaTotal = 0;
-  carrito.forEach(function (hamburguesa) {
-    sumaTotal += hamburguesa.precio;
+  carrito.forEach(function (producto) {
+    sumaTotal += producto.precio;
   });
   return sumaTotal;
 };
 
 var cardContainer = document.querySelector("#cajonContenedor");
-productos.forEach(function (producto) {
-  var cardP = document.createElement("div");
-  cardP.className = "card col-xs-12 col-lg-3 col-sm-3";
-  cardP.innerHTML = "  <p class=\"card-text\">".concat(producto.nombre, "</p>\n     <img src=\"../media/burguer.jpg\" class=\"p-2 card-img-top\" alt=\"...\">\n    <div class=\"card-body\">\n      <p class=\"card-text\">Ingredientes: ").concat(producto.descripcion, "</p>\n      <p class=\"card-text\">$").concat(producto.precio, "</p>\n      <button data-id=").concat(producto.id, " class=\"btn btn-warning\">Agregar</button>");
-  cardContainer.append(cardP);
-}); //
 
-var agregarProducto = function agregarProducto(e) {
-  var productoElegido = e.target.getAttribute("data-id");
-  var producto = productos.find(function (producto) {
-    return producto.id == productoElegido;
+var renderizadoDeProductos = function renderizadoDeProductos() {
+  productos.forEach(function (producto) {
+    var cardP = document.createElement("div");
+    cardP.className = "card col-xs-12 col-lg-3 col-sm-3";
+    cardP.innerHTML = "  <p class=\"card-text\">".concat(producto.nombre, "</p>\n     <img src=").concat(producto.img, "  class=\"p-2 card-img-top\" alt=\"...\">\n    <div class=\"card-body\">\n      <p class=\"card-text\">Ingredientes: ").concat(producto.descripcion, "</p>\n      <p class=\"card-text\">$").concat(producto.precio, "</p>");
+    var botonP = document.createElement("button");
+    botonP.setAttribute("data-id", producto.id);
+    botonP.className = "btn btn-warning";
+    botonP.innerText = "Agregar";
+    cardContainer.append(cardP);
+    cardP.append(botonP);
   });
-  carrito.push(producto);
-  localStorage.setItem("carrito", JSON.stringify(carrito));
+  listenerbotones();
 };
 
-var botonesDeAgregar = document.querySelectorAll(".btn");
-botonesDeAgregar.forEach(function (botonCompra) {
-  botonCompra.addEventListener("click", mostrarMensaje);
+var carritoContainer = document.querySelector("#carrito");
+
+var renderizadoCarrito = function renderizadoCarrito() {
+  carrito.forEach(function (producto) {
+    var cardP = document.createElement("div");
+    cardP.className = "card col-xs-12 col-lg-3 col-sm-3";
+    cardP.innerHTML = "  <p class=\"card-text\">".concat(producto.nombre, "</p>\n     <img src=").concat(producto.img, "  class=\"p-2 card-img-top\" alt=\"...\">\n    <div class=\"card-body\">\n      <p class=\"card-text\">Ingredientes: ").concat(producto.descripcion, "</p>\n      <p class=\"card-text\">$").concat(producto.precio, "</p>");
+    carritoContainer.append(cardP);
+  });
+};
+
+var botonAgregar = function botonAgregar(e) {
+  var productoIdElegido = e.target.getAttribute("data-id");
+  var productoElegido = productos.find(function (producto) {
+    return productoIdElegido == producto.id;
+  });
+
+  var mostrarMensaje = function mostrarMensaje() {
+    Swal.fire({
+      title: 'Desea Llevarla?',
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: 'Quiero llevarla',
+      confirmButtonColor: "#22D619",
+      denyButtonText: "Mejor no",
+      denyButtonColor: "#EA1818"
+    }).then(function (result) {
+      if (result.isConfirmed) {
+        Toastify({
+          text: "Producto Agregado",
+          className: "info",
+          style: {
+            background: "linear-gradient(to right, #22D619, #52D84B)"
+          }
+        }).showToast();
+        carrito.push(productoElegido);
+        localStorage.setItem('carrito', JSON.stringify(carrito));
+        console.log(carrito);
+      } else if (result.isDenied) {
+        Toastify({
+          text: "no se ha agregado el producto",
+          className: "info",
+          style: {
+            background: "linear-gradient(to right, #EA1818, #C83939)"
+          }
+        }).showToast();
+      }
+    });
+  };
+
+  mostrarMensaje();
+};
+
+var listenerbotones = function listenerbotones() {
+  var botonesDeAgregar = document.querySelectorAll(".btn");
+  botonesDeAgregar.forEach(function (botonCompra) {
+    botonCompra.addEventListener("click", botonAgregar);
+  });
+};
+
+var busqueda = document.querySelector("#inputSearch");
+
+var search = function search(productos) {
+  productos.find(productos ? console.log(productoElegido) : productos.nombre);
+};
+
+fetch('../productos.json').then(function (res) {
+  return res.json();
+}).then(function (data) {
+  productos = data;
+  renderizadoDeProductos(data);
 });
--- Js/main.js
+++ Js/main.js
@@ -1,107 +1,132 @@
-const renderizadoDeProductos = (data) => {
-    console.log(data);
-}
-
-fetch('../productos.json')
-    .then((res) => res.json())
-    .then((data) => {
-        renderizadoDeProductos(data)
-    })
 
 
-class Hamburguesa {
-    constructor(id, nombre, descripcion, precio) {
-        this.id = id
-        this.nombre = nombre
-        this.descripcion = descripcion
-        this.precio = precio
-    }
-}
+let carrito = []
 
-const hamburguesa1 = new Hamburguesa(101, "hamburguesa de carne", "pan, carne, tomate, lechuga, salsas", 7990)
-const hamburguesa2 = new Hamburguesa(102, "hamburguesa de pollo", "pan, pollo, tomate, lechuga, salsas", 6990)
-const hamburguesa3 = new Hamburguesa(103, "hamburguesa de chuleta", "pan, chuleta, tomate, lechuga, salsas", 8990)
-const hamburguesa4 = new Hamburguesa(104, "hamburguesa mixta", "pan, carne, pollo y/o chuleta, tomate, lechuga, salsas", 10990)
+let productos = []
 
-const carrito = []
-
-const productos = [hamburguesa1, hamburguesa2, hamburguesa3, hamburguesa4]
-
-const mostrarMensaje = () => {
-    Swal.fire({
-        title: 'Desea Llevarla?',
-        showDenyButton: true,
-        showCancelButton: true,
-        confirmButtonText: 'Quiero llevarla',
-        confirmButtonColor: "#22D619",
-        denyButtonText: `Mejor no`,
-        denyButtonColor: "#EA1818"
-    }).then((result) => {
-        /* Read more about isConfirmed, isDenied below */
-        if (result.isConfirmed) {
-            Toastify({
-                text: "Producto Agregado",
-                className: "info",
-                style: {
-                    background: "linear-gradient(to right, #22D619, #52D84B)",
-                }
-            }).showToast();
-
-
-        } else if (result.isDenied) {
-            Toastify({
-                text: "no se ha agregado el producto",
-                className: "info",
-                style: {
-                    background: "linear-gradient(to right, #EA1818, #C83939)",
-                }
-            }).showToast();
-        }
-    })
-
-}
 
 const totalCarrito = () => {
     let sumaTotal = 0
-    carrito.forEach((hamburguesa) => {
-        sumaTotal += hamburguesa.precio
+    carrito.forEach((producto) => {
+        sumaTotal += producto.precio
     })
     return sumaTotal
 }
 
 const cardContainer = document.querySelector("#cajonContenedor")
 
-productos.forEach((producto) => {
-    const cardP = document.createElement("div")
-    cardP.className = "card col-xs-12 col-lg-3 col-sm-3"
-    cardP.innerHTML = `  <p class="card-text">${producto.nombre}</p>
-     <img src="../media/burguer.jpg" class="p-2 card-img-top" alt="...">
+const renderizadoDeProductos = () => {
+
+    productos.forEach((producto) => {
+
+        const cardP = document.createElement("div")
+        cardP.className = "card col-xs-12 col-lg-3 col-sm-3"
+        cardP.innerHTML = `  <p class="card-text">${producto.nombre}</p>
+     <img src=${producto.img}  class="p-2 card-img-top" alt="...">
     <div class="card-body">
       <p class="card-text">Ingredientes: ${producto.descripcion}</p>
-      <p class="card-text">$${producto.precio}</p>
-      <button data-id=${producto.id} class="btn btn-warning">Agregar</button>`
+      <p class="card-text">$${producto.precio}</p>`
+        const botonP = document.createElement("button")
+        botonP.setAttribute("data-id", producto.id)
+        botonP.className = "btn btn-warning"
+        botonP.innerText = "Agregar"
 
-    cardContainer.append(cardP)
+        cardContainer.append(cardP)
+        cardP.append(botonP)
 
-})
 
-//
+    })
 
-const agregarProducto = (e) => {
+    listenerbotones()
 
-    const productoElegido = e.target.getAttribute("data-id")
-    const producto = productos.find((producto) => producto.id == productoElegido)
-    carrito.push(producto);
-    localStorage.setItem("carrito", JSON.stringify(carrito))
+
+}
+const carritoContainer = document.querySelector("#carrito")
+
+const renderizadoCarrito = () => {
+
+    carrito.forEach((producto) => {
+
+        const cardP = document.createElement("div")
+        cardP.className = "card col-xs-12 col-lg-3 col-sm-3"
+        cardP.innerHTML = `  <p class="card-text">${producto.nombre}</p>
+     <img src=${producto.img}  class="p-2 card-img-top" alt="...">
+    <div class="card-body">
+      <p class="card-text">Ingredientes: ${producto.descripcion}</p>
+      <p class="card-text">$${producto.precio}</p>`
+
+        carritoContainer.append(cardP)
+
+
+    })
+}
+
+
+const botonAgregar = (e) => {
+    const productoIdElegido = e.target.getAttribute("data-id")
+    const productoElegido = productos.find((producto) => productoIdElegido == producto.id)
+
+    const mostrarMensaje = () => {
+        Swal.fire({
+            title: 'Desea Llevarla?',
+            showDenyButton: true,
+            showCancelButton: true,
+            confirmButtonText: 'Quiero llevarla',
+            confirmButtonColor: "#22D619",
+            denyButtonText: `Mejor no`,
+            denyButtonColor: "#EA1818"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Toastify({
+                    text: "Producto Agregado", className: "info",
+                    style: {
+                        background: "linear-gradient(to right, #22D619, #52D84B)",
+                    },
+                }).showToast()
+                carrito.push(productoElegido)
+                localStorage.setItem('carrito', JSON.stringify(carrito))
+
+                console.log(carrito);
+            } else if (result.isDenied) {
+                Toastify({
+                    text: "no se ha agregado el producto",
+                    className: "info",
+                    style: {
+                        background: "linear-gradient(to right, #EA1818, #C83939)",
+                    }
+                }).showToast();
+            }
+
+        })
+    }
+    mostrarMensaje()
 
 }
 
 
-const botonesDeAgregar = document.querySelectorAll(".btn")
-botonesDeAgregar.forEach((botonCompra) => {
-    botonCompra.addEventListener("click", mostrarMensaje)
-})
+
+const listenerbotones = () => {
+    const botonesDeAgregar = document.querySelectorAll(".btn")
+    botonesDeAgregar.forEach((botonCompra) => {
+
+        botonCompra.addEventListener("click", botonAgregar)
+    })
 
 
+}
 
+const busqueda = document.querySelector("#inputSearch")
+
+const search = (productos) => {
+
+    productos.find(productos ? console.log(productoElegido) : productos.nombre)
+}
+
+fetch('../productos.json')
+    .then((res) => res.json())
+    .then((data) => {
+        productos = data
+        renderizadoDeProductos(data)
+
+    })
 
